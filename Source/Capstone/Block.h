1e0f3e632bddcfa926e5de1bec6916752271567c
Jevspr <jevelazquezs70@gmail.com>
2021-06-24 00:43:00 -0400

    *Compilar

---
+++ Source/Capstone/Block.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Block.generated.h"
+
+UCLASS()
+class CAPSTONE_API ABlock : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ABlock();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+		class UStaticMeshComponent* Block;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		class UBoxComponent* BoxCollision;
+
+	UFUNCTION()
+		void BeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
---
+++ Source/Capstone/PartyPlayerController.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PartyPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAPSTONE_API APartyPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+};
